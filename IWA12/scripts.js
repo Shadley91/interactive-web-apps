84411c63dc3a003089e2b5ef6a73249e0e887edc
Shadley Philander <shadley.phil@gmail.com>
2023-07-31 23:50:43 +0200

    inital commit - creates files

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -30,34 +30,34 @@ const STATUS_MAP = {
 // Edit below line
 
 const statusElement0 = document.querySelector("#book1 .status");
-const reserve0 = document.querySelector(".reserve");
-const checkout0 = document.querySelector(".checkout");
-const checkin0 = document.querySelector(".checkin");
+const reserve0 = document.querySelector("#book1 .reserve");
+const checkout0 = document.querySelector("#book1 .checkout");
+const checkin0 = document.querySelector("#book1 .checkin");
 
 const statusElement1 = document.querySelector("#book2 .status");
-const reserve1 = document.querySelector(".reserve");
-const checkout1 = document.querySelector(".checkout");
-const checkin1 = document.querySelector(".checkin");
+const reserve1 = document.querySelector("#book2 .reserve");
+const checkout1 = document.querySelector("#book2 .checkout");
+const checkin1 = document.querySelector("#book2 .checkin");
 
 const statusElement2 = document.querySelector("#book3 .status");
-const reserve2 = document.querySelector(".reserve");
-const checkout2 = document.querySelector(".checkout");
-const checkin2 = document.querySelector(".checkin");
+const reserve2 = document.querySelector("#book3 .reserve");
+const checkout2 = document.querySelector("#book3 .checkout");
+const checkin2 = document.querySelector("#book3 .checkin");
 
-checkin0.style.color = "none";
+checkin0.style.color = "black";
 statusElement0.style.color = STATUS_MAP.overdue.color;
-reserve0.enabled = STATUS_MAP.overdue.canReserve;
-checkout0.enabled = STATUS_MAP.overdue.canCheckout;
+reserve0.disabled = STATUS_MAP.overdue.canReserve;
+checkout0.disabled = STATUS_MAP.overdue.canCheckout;
 checkin0.disabled = STATUS_MAP.overdue.canCheckIn;
 
-checkin1.style.color = "none";
+checkin1.style.color = "black";
 statusElement1.style.color = STATUS_MAP.reserved.color;
 reserve1.disabled = STATUS_MAP.reserved.canReserve;
-checkout1.enabled = STATUS_MAP.reserved.canCheckout;
+checkout1.disabled = STATUS_MAP.reserved.canCheckout;
 checkin1.disabled = STATUS_MAP.reserved.canCheckIn;
 
-checkin2.style.color = "none";
+checkin2.style.color = "black";
 statusElement2.style.color = STATUS_MAP.shelf.color;
-reserve2.enabled = STATUS_MAP.shelf.canReserve;
-checkout2.enabled = STATUS_MAP.shelf.canCheckout;
+reserve2.disabled = STATUS_MAP.shelf.canReserve;
+checkout2.disabled = STATUS_MAP.shelf.canCheckout;
 checkin2.disabled = STATUS_MAP.shelf.canCheckIn;
--- IWA7/Challenge2script.js
+++ IWA7/Challenge2script.js
@@ -1,9 +1,10 @@
 const nickName = "Timmy";
 const firstName = "Timothy";
 
-if (!nickName && !firstName);
-{
+if (!nickName && !firstName) {
   console.log("Good Morning");
+} else {
+  console.log("Good Morning", nickName || firstName);
 }
-console.log("Good Morning", nickName || firstName);
+
 /* The reason for it logging both variables instead of the name is because it is evaulating both variables.*/
