77dd80124a6c9083daa55befd5862d002a216b0d
Shadley Philander <shadley.phil@gmail.com>
2023-08-13 17:21:05 +0200

    initial commits - creates files

--- IWA16/scripts.js
+++ IWA16/scripts.js
@@ -65,18 +65,26 @@ const data = {
   },
 };
 
+// Only edit below this comment
+
+// Function to generate HTML for athlete data
+
 const createHtml = (athlete) => {
   const { firstName, surname, id, races } = athlete;
   const latestRace = races[races.length - 1];
 
+  // Create a document fragment to build up HTML content
   const fragment = document.createDocumentFragment();
 
+  // Create an h2 element for the athlete's ID
   const title = document.createElement("h2");
   title.textContent = id;
   fragment.appendChild(title);
 
+  // Create a dl element for the athlete's details
   const list = document.createElement("dl");
 
+  // Create dt and dd elements for full name
   const fullNameDt = document.createElement("dt");
   fullNameDt.textContent = "Full Name";
   const fullNameDd = document.createElement("dd");
@@ -84,6 +92,7 @@ const createHtml = (athlete) => {
   list.appendChild(fullNameDt);
   list.appendChild(fullNameDd);
 
+  // Create dt and dd elements for number of races
   const racesDt = document.createElement("dt");
   racesDt.textContent = "Number of Races";
   const racesDd = document.createElement("dd");
@@ -91,6 +100,7 @@ const createHtml = (athlete) => {
   list.appendChild(racesDt);
   list.appendChild(racesDd);
 
+  // Create dt and dd elements for date of most recent race
   const dateDt = document.createElement("dt");
   dateDt.textContent = "Date of Most Recent Race";
   const dateDd = document.createElement("dd");
@@ -102,6 +112,7 @@ const createHtml = (athlete) => {
   list.appendChild(dateDt);
   list.appendChild(dateDd);
 
+  // Create dt and dd elements for duration of most recent race
   const durationDt = document.createElement("dt");
   durationDt.textContent = "Duration of Most Recent Race";
   const durationDd = document.createElement("dd");
@@ -119,6 +130,8 @@ const createHtml = (athlete) => {
   return fragment;
 };
 
+// Destructure athlete data
 const { NM372, SV782 } = data.response.data;
+
 document.querySelector("#NM372").appendChild(createHtml(NM372));
 document.querySelector("#SV782").appendChild(createHtml(SV782));
